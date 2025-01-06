caddc35aab8e7dce836653bc9d331af680bfe9a9
Dickson-Onyongo <dicksononyong@gmail.com>
2025-01-06 11:50:20 +0300

    New project setup, due to breakdown of the initial.

---
+++ hope-techv/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <p>Hope Tech Ventures</p>
+        </header>
+
+        <div className="App-body">
+          <p>HELLO WORLD</p>
+        </div>
+
+        <footer className="App-footer">
+          <p>I LOVE CODING</p>
+        </footer>
+      </div>
+    </>
+  );
+}
