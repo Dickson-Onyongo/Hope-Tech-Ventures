dec35248e51b283bbc0f97ebc18f7817b0349aea
Dickson-Onyongo <dicksononyong@gmail.com>
2025-01-07 23:59:06 +0300

    Navbar change of search button color.

--- hope-techv/src/components/Navbar.js
+++ hope-techv/src/components/Navbar.js
@@ -3,10 +3,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-      <div class="container-fluid text-white">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container-fluid text-white">
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarTogglerDemo03"
@@ -14,41 +14,41 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand text-white" href="#">
+        <a className="navbar-brand text-white" href="#">
           Hope Tech Ventures
         </a>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+            <li className="nav-item">
               <a
-                class="nav-link active text-white"
+                className="nav-link active text-white "
                 aria-current="page"
                 href="#"
               >
                 Home
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link text-white" href="#">
+            <li className="nav-item">
+              <a className="nav-link text-white ml-5" href="#">
                 Our Products
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link text-white" href="#" tabindex="-1">
-                Support
+            <li className="nav-item">
+              <a className="nav-link text-white ml-5" href="#" tabindex="-1">
+                Contact us
               </a>
             </li>
           </ul>
-          <form class="d-flex">
+          <form className="d-flex">
             <input
-              class="form-control me-2"
+              className="form-control me-2 ml-5"
               type="search"
               placeholder="Search"
               aria-label="Search"
             ></input>
-            <button class="btn btn-outline-success" type="submit">
+            <button className="btn btn-outline-primary" type="submit">
               Search
             </button>
           </form>
