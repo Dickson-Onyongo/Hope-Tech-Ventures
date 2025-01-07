d975477d708cab3f0e75b0497703b82841900cb5
Dickson-Onyongo <dicksononyong@gmail.com>
2025-01-07 23:23:52 +0300

    Navbar modification,change of colors and addition of the search tab.

--- hope-techv/src/App.js
+++ hope-techv/src/App.js
@@ -1,38 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import background from "./assets/background-1.png";
+import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { Container, Row, Col } from "react-bootstrap";
-
-export default function App() {
-  return (
-    <>
-      <div className="App">
-        <header className="App-header">
-          <p>Hope Tech Ventures</p>
-        </header>
-
-        <main id="main-body">
-          <Container>
-            <Row className=" first-row justify-content-center  text-md-start">
-              <h1>HOPE TECH VENTURES</h1>
-              <p>Solutions at your finger tips.</p>
-            </Row>
-
-            <Row>
-              <Col>
-                <h2>Its all about the latest and greatest solutions </h2>
-              </Col>
-              <Col></Col>
-            </Row>
-          </Container>
-        </main>
-
-        <footer className="App-footer">
-          <p>I LOVE CODING</p>
-        </footer>
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
       </div>
-    </>
-  );
+    );
+  }
 }
+
+export default App;
--- hope-techv/src/components/Navbar.js
+++ hope-techv/src/components/Navbar.js
@@ -1,43 +1,57 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <div class="container-fluid text-white">
         <button
           class="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
+          data-bs-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">
-              Home
-            </a>
-            <a class="nav-link" href="#">
-              Features
-            </a>
-            <a class="nav-link" href="#">
-              Pricing
-            </a>
-            <a
-              class="nav-link disabled"
-              href="#"
-              tabindex="-1"
-              aria-disabled="true"
-            >
-              Disabled
-            </a>
-          </div>
+        <a class="navbar-brand text-white" href="#">
+          Hope Tech Ventures
+        </a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a
+                class="nav-link active text-white"
+                aria-current="page"
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">
+                Our Products
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#" tabindex="-1">
+                Support
+              </a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            ></input>
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
         </div>
       </div>
     </nav>
